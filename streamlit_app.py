ffbfd0c2bb76daa4ac29479fcef4b1c3d60e9567
raghuveernama <124514654+raghuveernama@users.noreply.github.com>
2023-02-05 16:26:01 -0500

    Update streamlit_app.py
    Updated the order of the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 import streamlit
 import pandas
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-#Lets put a pick list here so they can pick the fruit they want to include
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -17,10 +11,14 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#Lets put a pick list here so they can pick the fruit they want to include
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
