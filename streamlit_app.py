939684b7888edaafe39f65593474b590e9833a89
raghuveernama <124514654+raghuveernama@users.noreply.github.com>
2023-02-05 20:55:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -39,3 +40,4 @@ streamlit.dataframe(my_data_row)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('Thanks for adding jackfruit')
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
