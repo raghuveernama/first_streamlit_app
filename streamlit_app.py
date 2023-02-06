8f51f6e5b27bf5ad9cba470f7bc16505646fc178
raghuveernama <124514654+raghuveernama@users.noreply.github.com>
2023-02-05 20:47:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,4 @@ streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_row)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+streamlit.write('Thanks for adding jackfruit')
